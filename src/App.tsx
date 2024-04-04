fd075d00f77d0397d2084a108eef2ecbef3a0172
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2024-04-04 21:16:26 +0200

    LP-12: component Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css';
+
 import { Header } from './components/Header';
 
-export const App = () => {
-
-  return (
+export const App = () => (
     <>
       <Header/>
     </>
   );
-};
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,28 @@
-import { Button } from 'primereact/button'; 
+import { Button } from 'primereact/button';
 import { Menubar } from 'primereact/menubar';
-import { MenuItem } from 'primereact/menuitem';
-import { menuItems } from './constants'
+
+import { menuItems } from './constants';
 
 export const Header = () => {
-    const items: MenuItem[] = menuItems;
-    const logo = <img alt="logo" src="https://primefaces.org/cdn/primereact/images/logo.png" height="40" className="mr-5"></img>;
-        const headerActions = (
-            <div className="flex align-items-center gap-2">
-                <Button className="mr-5" label="Пробний урок" />
-                <Button label="Мова" />
-            </div>
-        );
-    
-    return (
-        <div className="card">
-            <Menubar model={items} start={logo} end={headerActions}/>
+    const items = menuItems;
+    const logo = (
+        <img 
+            alt="logo" 
+            src="https://primefaces.org/cdn/primereact/images/logo.png" 
+            height="40" 
+            className="mr-5"
+        />
+    );
+    const headerActions = (
+        <div className="flex align-items-center gap-2">
+            <Button className="mr-5" label="Пробний урок" />
+            <Button label="Мова" />
         </div>
     );
-}
-   
+
+    return (
+        <div className="card">
+            <Menubar model={items} start={logo} end={headerActions} />
+        </div>
+    );
+};
