d25e2bb450192e865cd3dace81e127ccc425f37d
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2024-04-12 20:53:17 +0200

    LP-14: code review

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,24 +4,20 @@ import { Menubar } from 'primereact/menubar';
 import { menuItems } from './constants';
 
 export const Header = () => {
+  const logo = (
+    <img
+      alt="logo"
+      src="https://primefaces.org/cdn/primereact/images/logo.png"
+      height="40"
+      className="mr-5"
+    />
+  );
+  const headerActions = (
+    <div className="flex justify-content-end gap-2">
+      <Button label="Пробний урок" />
+      <Button label="Мова" />
+    </div>
+  );
 
-    const logo = (
-        <img 
-            alt="logo" 
-            src="https://primefaces.org/cdn/primereact/images/logo.png" 
-            height="40" 
-            className="mr-5"
-        />
-    );
-    const headerActions = (
-        <div className="flex justify-content-end gap-2">
-            <Button label="Пробний урок" />
-            <Button label="Мова" />
-        </div>
-    );
-
-    return (
-            <Menubar className='text-xl'  model={menuItems} start={logo} end={headerActions} />
-    );
+  return <Menubar className="text-xl" model={menuItems} start={logo} end={headerActions} />;
 };
-
--- src/components/SiteInfo/SiteInfo.tsx
+++ src/components/SiteInfo/SiteInfo.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import { Card } from 'primereact/card';
-import infoCardImg from '../../../public/images/Info.jpg';
 import { Button } from 'primereact/button';
+import { Card } from 'primereact/card';
 
-
+import styles from './styles.module.css';
 
 export const SiteInfo = () => {
-    const header = (
-        <img className='max-h-30rem bg-no-repeat bg-center bg-cover' alt="Card" src={infoCardImg} />
-    );
+  const header = (
+    <div className={styles.bgHeaderCard}>
+      <div className="flex flex-column align-items-end pt-8 pr-5">
+        <p className="text-100 text-2xl">Запишись до наc та почни навчатися вже сьогодні</p>
+        <Button label="Записатися" />
+      </div>
+    </div>
+  );
 
-    return <Card title="Advanced Card" subTitle="Card subtitle" header={header} className="relative">
-        <p className="m-0">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
-            numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
-        </p>
-        <div className='absolute top-0 right-0'>
-            <p className='p-4 border-round font-bold text-gray-800'>Запишись до наш та почни навчатися вже сьогодні</p>
-            <Button label="Записатися" />
-        </div>
-    </Card>;
+  return (
+    <Card title="Advanced Card" subTitle="Card subtitle" header={header}>
+      <p className="m-0">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error
+        repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa
+        ratione quam perferendis esse, cupiditate neque quas!
+      </p>
+    </Card>
+  );
 };
