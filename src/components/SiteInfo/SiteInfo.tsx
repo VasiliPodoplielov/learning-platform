21f90eaadf67fd34b272e9f3eccf7208741c3bdc
Maksym Pysmennyi <109579513+MaksymPi@users.noreply.github.com>
2024-04-20 19:13:17 +0200

    LP-14-add-section-about-school (#14)
    LP-14-add-section-about-school

--- src/components/SiteInfo/SiteInfo.tsx
+++ src/components/SiteInfo/SiteInfo.tsx
@@ -1,28 +1,28 @@
 import { Button } from 'primereact/button';
 import { Card } from 'primereact/card';
+import { useTranslation } from 'react-i18next';
 
 import styles from './styles.module.css';
 
 export const SiteInfo = () => {
+  const { t } = useTranslation();
+
   const header = (
     <div className={styles.bgHeaderCard}>
       <div className="flex flex-column align-items-end pt-8 pr-5">
-        <p className="text-100 text-2xl">
-          Запишись до наc та почни навчатися вже сьогодні
-        </p>
-        <Button label="Записатися" />
+        <p className="text-100 text-2xl">{t('siteInfoSection.textSignUp')}</p>
+        <Button label={t('buttons.signUpClass')} />
       </div>
     </div>
   );
 
   return (
-    <Card title="Advanced Card" subTitle="Card subtitle" header={header}>
-      <p className="m-0">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed
-        consequuntur error repudiandae numquam deserunt quisquam repellat libero
-        asperiores earum nam nobis, culpa ratione quam perferendis esse,
-        cupiditate neque quas!
-      </p>
+    <Card
+      title={t('siteInfoSection.titleInfo')}
+      subTitle={t('siteInfoSection.subTitleInfo')}
+      header={header}
+    >
+      <p className="m-0">{t('siteInfoSection.textInfo')}</p>
     </Card>
   );
 };
