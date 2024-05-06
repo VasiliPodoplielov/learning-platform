174dba8ba9a6db9d2f17a2cc6fa7a96a1ab7ceb5
Vasyl Podoplielov <37293464+VasiliPodoplielov@users.noreply.github.com>
2024-05-06 19:19:04 +0200

    fix: fix image issue (#22)
    Fix image issue

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react()],
   base: '/learning-platform/',
+  resolve: {
+    alias: {
+      src: '/src',
+    },
+  },
 });
