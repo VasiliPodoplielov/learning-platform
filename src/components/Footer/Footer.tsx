ede21a405e21d9d22d571b6d84c7ea453ab4068a
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2024-05-22 19:54:29 +0200

    code review

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -11,21 +11,21 @@ export const Footer = () => {
     <div className="card flex flex-row justify-content-around p-2 bg-bluegray-900">
       <div className="flex flex-column align-items-center">
         <img alt="logo" src="https://primefaces.org/cdn/primereact/images/logo.png" height="60" />
-        <ul className="flex flex-row justify-content-around">
+        <ul className="flex flex-row justify-content-around list-none">
           <li className="p-3">
-            <a href="https://www.facebook.com" target="_blank">
+            <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
               <i className="pi pi-facebook" aria-hidden="true"></i>{' '}
               <span className="sr-only">Facebook</span>
             </a>
           </li>
           <li className="p-3">
-            <a href="https://www.instagram.com" target="_blank">
+            <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
               <i className="pi pi-instagram" aria-hidden="true"></i>{' '}
               <span className="sr-only">Instagram</span>
             </a>
           </li>
           <li className="p-3">
-            <a href="https://www.linkedin.com" target="_blank">
+            <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">
               <i className="pi pi-linkedin" aria-hidden="true"></i>{' '}
               <span className="sr-only">LinkedIn</span>
             </a>
--- src/components/Footer/dataHelpers.ts
+++ src/components/Footer/dataHelpers.ts
@@ -1,54 +1,52 @@
-export const getFooterItems = (getTranslation: (key: string) => string) => {
-  return {
-    links: [
-      {
-        label: getTranslation('header.menu.home'),
-        icon: 'pi pi-home',
-      },
-      {
-        label: getTranslation('header.menu.reviews'),
-        icon: 'pi pi-star',
-      },
-      {
-        label: getTranslation('header.menu.teachers'),
-        icon: 'pi pi-users',
-      },
-      {
-        label: getTranslation('header.menu.contacts'),
-        icon: 'pi pi-envelope',
-      },
-    ],
-    contacts: [
-      {
-        label: getTranslation('footer.contacts.tittle'),
-        icon: '',
-      },
-      {
-        label: '+38(067)043-45-56',
-        icon: 'pi pi-phone',
-      },
-      {
-        label: 'shool@gmail.com',
-        icon: 'pi pi-envelope',
-      },
-      {
-        label: getTranslation('footer.contacts.telegram'),
-        icon: 'pi pi-telegram',
-      },
-    ],
-    address: [
-      {
-        label: getTranslation('footer.address.tittle'),
-      },
-      {
-        label: getTranslation('footer.address.timeOpen'),
-      },
-      {
-        label: getTranslation('footer.address.tittleAddress'),
-      },
-      {
-        label: getTranslation('footer.address.addressStreet'),
-      },
-    ],
-  };
-};
+export const getFooterItems = (getTranslation: (key: string) => string) => ({
+  links: [
+    {
+      label: getTranslation('header.menu.home'),
+      icon: 'pi pi-home',
+    },
+    {
+      label: getTranslation('header.menu.reviews'),
+      icon: 'pi pi-star',
+    },
+    {
+      label: getTranslation('header.menu.teachers'),
+      icon: 'pi pi-users',
+    },
+    {
+      label: getTranslation('header.menu.contacts'),
+      icon: 'pi pi-envelope',
+    },
+  ],
+  contacts: [
+    {
+      label: getTranslation('footer.contacts.tittle'),
+      icon: '',
+    },
+    {
+      label: '+38(067)043-45-56',
+      icon: 'pi pi-phone',
+    },
+    {
+      label: 'shool@gmail.com',
+      icon: 'pi pi-envelope',
+    },
+    {
+      label: getTranslation('footer.contacts.telegram'),
+      icon: 'pi pi-telegram',
+    },
+  ],
+  address: [
+    {
+      label: getTranslation('footer.address.tittle'),
+    },
+    {
+      label: getTranslation('footer.address.timeOpen'),
+    },
+    {
+      label: getTranslation('footer.address.tittleAddress'),
+    },
+    {
+      label: getTranslation('footer.address.addressStreet'),
+    },
+  ],
+});
