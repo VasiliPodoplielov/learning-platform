d410f52bc36a27e27de3aa92e777a33cd3e6278e
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2024-05-22 16:32:36 +0200

    LP-21-Create-footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,40 @@
+import { Menu } from 'primereact/menu';
+import { useTranslation } from 'react-i18next';
+
+import { getFooterItems } from './dataHelpers';
+
+export const Footer = () => {
+  const { t } = useTranslation();
+  const { links, contacts, address } = getFooterItems(t);
+
+  return (
+    <div className="card flex flex-row justify-content-around p-2 bg-bluegray-900">
+      <div className="flex flex-column align-items-center">
+        <img alt="logo" src="https://primefaces.org/cdn/primereact/images/logo.png" height="60" />
+        <ul className="flex flex-row justify-content-around">
+          <li className="p-3">
+            <a href="https://www.facebook.com" target="_blank">
+              <i className="pi pi-facebook" aria-hidden="true"></i>{' '}
+              <span className="sr-only">Facebook</span>
+            </a>
+          </li>
+          <li className="p-3">
+            <a href="https://www.instagram.com" target="_blank">
+              <i className="pi pi-instagram" aria-hidden="true"></i>{' '}
+              <span className="sr-only">Instagram</span>
+            </a>
+          </li>
+          <li className="p-3">
+            <a href="https://www.linkedin.com" target="_blank">
+              <i className="pi pi-linkedin" aria-hidden="true"></i>{' '}
+              <span className="sr-only">LinkedIn</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <Menu style={{ background: 'none', border: 'none' }} model={links} />
+      <Menu style={{ background: 'none', border: 'none' }} model={contacts} />
+      <Menu style={{ background: 'none', border: 'none' }} model={address} />
+    </div>
+  );
+};
