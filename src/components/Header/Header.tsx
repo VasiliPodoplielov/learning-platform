ca5260cf49bd40a77b543a9255d7630e7607e21c
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2024-08-01 15:35:30 +0200

    header_desing

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,10 @@
 import { Button } from 'primereact/button';
 import { Menubar } from 'primereact/menubar';
 import { useTranslation } from 'react-i18next';
+import logoImage from 'src/assets/images/logo.png';
 
 import { LanguageSwitcher } from '../LanguageSwitcher';
 import { getMenuItems } from './dataHelpers';
-
-import logoImage from 'src/assets/images/logo.png';
 
 export const Header = () => {
   const { t } = useTranslation();
