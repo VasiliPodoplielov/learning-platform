e985816a2c3875ef256b9823d8be3e652a7b7fe9
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2024-08-16 22:21:10 +0200

    breakpoint and border header

--- src/components/LanguageSwitcher/dataHelpers.tsx
+++ src/components/LanguageSwitcher/dataHelpers.tsx
@@ -8,6 +8,6 @@ export const countryOptionTemplate = (country: string) => (
       className={`mr-2 flag flag-${country}`}
       style={{ width: '18px' }}
     />
-    <div>{country}</div>
+    <div className="languageSwitcherCountry">{country}</div>
   </div>
 );
