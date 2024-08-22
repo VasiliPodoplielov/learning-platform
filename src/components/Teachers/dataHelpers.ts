10e92cc8ba31aa52f18f95c5da1d66e42f65df73
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2024-08-22 16:01:31 +0200

    code review

--- src/components/Teachers/dataHelpers.ts
+++ src/components/Teachers/dataHelpers.ts
@@ -1,27 +1,27 @@
-import Anastasiia from 'src/assets/images/teachers/TeacherAnastasiaImg.png';
-import Diana from 'src/assets/images/teachers/TeacherDianaImg.png';
-import Sofiia from 'src/assets/images/teachers/TeacherSofiiaImg.png';
+import TeacherAnastasiaImg from 'src/assets/images/teachers/TeacherAnastasiaImg.png';
+import TeacherDianaImg from 'src/assets/images/teachers/TeacherDianaImg.png';
+import TeacherSofiiaImg from 'src/assets/images/teachers/TeacherSofiiaImg.png';
 
 import { Teacher } from './types.ts';
 
 export const getTeachers = (getTranslation: (key: string) => string): Teacher[] => [
   {
     id: '1',
-    imageUrl: Sofiia,
+    imageUrl: TeacherSofiiaImg,
     name: 'Sofiia',
     language: getTranslation('teachersSection.teacherSofiia.language'),
     introduction: getTranslation('teachersSection.teacherSofiia.introduction'),
   },
   {
     id: '2',
-    imageUrl: Anastasiia,
+    imageUrl: TeacherAnastasiaImg,
     name: 'Anastasiia',
     language: getTranslation('teachersSection.teacherAnastasiia.language'),
     introduction: getTranslation('teachersSection.teacherAnastasiia.introduction'),
   },
   {
     id: '3',
-    imageUrl: Diana,
+    imageUrl: TeacherDianaImg,
     name: 'Diana',
     language: getTranslation('teachersSection.teacherDiana.language'),
     introduction: getTranslation('teachersSection.teacherDiana.introduction'),
