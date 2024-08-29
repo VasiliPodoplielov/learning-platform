9e4c813270dee5f5d6392b42b19e4ac877212216
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2024-08-29 10:25:15 +0200

    code review

--- src/components/SiteInfo/SiteInfo.tsx
+++ src/components/SiteInfo/SiteInfo.tsx
@@ -2,12 +2,13 @@ import { Button } from 'primereact/button';
 import { Card } from 'primereact/card';
 import { useTranslation } from 'react-i18next';
 
+import sharedStyles from '../styles/sharedStyles.module.css';
 import styles from './styles.module.css';
 
 export const SiteInfo = () => {
   const { t } = useTranslation();
 
-  const customHeader = (
+  const header = (
     <div>
       <h1 className="custom-title-class pb-5">{t('siteInfoSection.titleInfo')}</h1>
       <h3 className="m-5 pb-7">{t('siteInfoSection.subTitleInfo')}</h3>
@@ -16,12 +17,11 @@ export const SiteInfo = () => {
 
   return (
     <div
-      className={`headerSection flex-column flex justify-content-center h-screen m-0 p-0 ${styles.bgHeaderCard}`}
+      className={`flex-column flex justify-content-center h-screen m-0 p-0 ${styles.bgHeaderCard}`}
     >
       <Card
-        className="pt-8 pb-7 text-center border-noround shadow-none"
-        style={{ background: 'transparent' }}
-        header={customHeader}
+        className={`pt-8 pb-7 text-center border-noround shadow-none ${sharedStyles.bgTransparent}`}
+        header={header}
       >
         <Button className="mt-2 uppercase letterSpacing_2" label={t('buttons.signUpClass')} />
       </Card>
