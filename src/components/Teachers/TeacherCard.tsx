9a1031e1aa5407d6b4ed2d556d7fba50402d52aa
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2024-09-25 20:06:37 +0200

    desing teachers section

--- src/components/Teachers/TeacherCard.tsx
+++ src/components/Teachers/TeacherCard.tsx
@@ -1,43 +1,32 @@
 import { Card } from 'primereact/card';
-import { useState } from 'react';
-
-import { Teacher } from './types';
 
 import sharedStyles from '../styles/sharedStyles.module.css';
-
+import styles from './styles.module.css';
+import { Teacher } from './types';
 interface Props {
   teacher: Teacher;
 }
 
 export const TeacherCard = ({ teacher }: Props) => {
   const { imageUrl, name, language, introduction } = teacher;
-  const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <Card
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      className="border-transparent"
-    >
-      <div className="w-full relative ">
+    <Card className="border-transparent">
+      <div className={`w-full relative ${styles.wrapperCard}`}>
         <img
           src={imageUrl}
           alt="Teacher"
           className={`h-14rem w-full h-auto border-round-2xl ${sharedStyles.objectFitCover} lg:h-full`}
         />
-        <div
-          className={`absolute top-0 left-0 w-full h-full flex flex-column justify-end p-3 text-justify transition-duration-300 border-radius ${isHovered ? 'bg-black-alpha-50 border-round-2xl' : 'bgTransparent'}`}
-        >
-          <div
-            className={`absolute top-0 left-0 w-full h-full flex flex-column justify-end p-3 text-justify ${isHovered ? 'bg-bg-black-alpha-50' : 'bgTransparent'}`}
-          >
+        <div className="absolute top-0 left-0 w-full h-full flex flex-column justify-end p-3 text-justify transition-duration-300 border-radius hover:bg-black-alpha-50 border-round-2xl">
+          <div className="absolute top-0 left-0 w-full h-full flex flex-column justify-end p-3 text-justify hover:bg-bg-black-alpha-50">
             <h3 className="text-white p-0 m-0">{name}</h3>
             <h4 className="text-white p-0 m-0">{language}</h4>
-            {isHovered && (
-              <p className="text-xs text-white text-overflow-clip w-full sm:text-sm xl:text-base">
-                {introduction}
-              </p>
-            )}
+            <p
+              className={`${styles.introductionText} text-xs text-white text-overflow-clip w-full sm:text-sm xl:text-base opacity-0 transition-opacity duration-300`}
+            >
+              {introduction}
+            </p>
           </div>
         </div>
       </div>
