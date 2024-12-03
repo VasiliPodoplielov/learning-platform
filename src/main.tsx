f2c73362fd9369958aa596753c9abdc25124a95f
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2024-12-03 17:39:41 +0100

    code review

--- src/main.tsx
+++ src/main.tsx
@@ -5,18 +5,17 @@ import 'primereact/resources/themes/vela-orange/theme.css';
 import './index.css';
 import './i18n/i18n.ts';
 
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { App } from './App.tsx';
 import { Header } from './components/Header/Header.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/learning-platform">
+  <Router basename="/learning-platform">
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/translations" element={<Header />} />
     </Routes>
-  </BrowserRouter>,
+  </Router>,
 );
