591ae28aaf94addfc6aa9e00c6887586bf28f252
Maksym Pysmennyi <109579513+MaksymPi@users.noreply.github.com>
2024-12-05 20:03:26 +0100

    LP-33: Add react router (#37)

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,17 @@ import 'primereact/resources/themes/vela-orange/theme.css';
 import './index.css';
 import './i18n/i18n.ts';
 
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { App } from './App.tsx';
+import { Header } from './components/Header/Header.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter basename="/learning-platform">
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/translations" element={<Header />} />
+    </Routes>
+  </BrowserRouter>,
 );
