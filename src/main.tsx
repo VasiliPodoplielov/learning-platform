52b9409a00e777410ca4edde894dea276ce4ee3e
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2025-01-10 19:57:02 +0100

    add translate page

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import 'primeflex/primeflex.css';
 import 'primereact/resources/themes/vela-orange/theme.css';
 import './index.css';
 import './i18n/i18n.ts';
-import { ReactSVG } from 'react-svg';
 
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -14,7 +13,6 @@ import { TranslatePage } from './components/Translate/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter basename="/learning-platform">
-    <ReactSVG src="svg.svg" />
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/translations" element={<TranslatePage />} />
