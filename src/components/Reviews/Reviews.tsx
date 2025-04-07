c27855220b544f2dc6a68949e7b0b7f2e45eb145
Maksym Pysmennyi <maksimviktorovichua@gmail.com>
2025-04-07 14:29:51 +0200

    code review

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -10,21 +10,21 @@ import { getReviews } from './dataHelpers';
 import { ReviewCard } from './ReviewCard';
 import { Review } from './types';
 
+const chunkArray = <T,>(arr: T[], size: number): T[][] =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (_, i) =>
+    arr.slice(i * size, i * size + size),
+  );
+
 export const Reviews = () => {
   const { t } = useTranslation();
   const reviews = getReviews(t);
-
-  const groupedReviews: Review[][] = [];
-  for (let i = 0; i < reviews.length; i += 3) {
-    groupedReviews.push(reviews.slice(i, i + 3));
-  }
 
   return (
     <div id="reviews" className="h-screen card bg-black pt-8">
       <h2 className="text-6xl text-center">{t('reviewsSection.titleSection')}</h2>
       <h3 className="text-1xl text-center text-gray-400">{t('reviewsSection.subTitleSection')}</h3>
       <Carousel
-        value={groupedReviews}
+        value={chunkArray(reviews, 3)}
         numVisible={REVIEWS_VISIBLE_ITEMS_COUNT}
         numScroll={REVIEWS_SCROLL_ITEMS_COUNT}
         circular={true}
